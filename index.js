d9260626ba525fd789fbed89199f8978f3c43561
Zahroul Ulum <moh.zahroul@gmail.com>
2017-11-28 10:39:09 +0700

    Update the app runner Add style, script, and data files reader that needed to be loaded with the app.

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const http = require('http');
 const fs = require('fs');
 
-http.createServer((request, response) => {
-  fs.readFile(`${__dirname}/index.html`, (error, data) => {
-    response.writeHeader(200, { 'Content-Type': 'text/html' });
+function readFile(filePath, response, contentType) {
+  fs.readFile(filePath, (error, data) => {
+    response.writeHeader(200, { 'Content-Type': contentType });
     response.end(data);
   });
+}
+
+http.createServer((request, response) => {
+  const requestUrl = request.url;
+
+  switch (requestUrl) {
+    case '/css/app.css':
+      readFile(`${__dirname}${requestUrl}`, response, 'text/css');
+      break;
+    case '/css/app.css.map':
+      readFile(`${__dirname}${requestUrl}`, response, 'application/json');
+      break;
+    case '/app.js':
+      readFile(`${__dirname}${requestUrl}`, response, 'text/javascript');
+      break;
+    case '/cafe-data.json':
+      readFile(`${__dirname}${requestUrl}`, response, 'application/json');
+      break;
+    default:
+      readFile(`${__dirname}/index.html`, response, 'text/html');
+  }
 }).listen(process.env.PORT);
